a7b0cec01d64aadd90e64e4023e34a7c66e464cf
Juan Carrasco <juan.carrasco@cyttek.com>
2022-07-24 14:43:10 -0400

    First load!

---
+++ Jmcarrasc0.Cryptography/Cryptography.cs
@@ -0,0 +1,201 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Jmcarrasc0.Cryptography
+{
+    public class Cryptography
+    {
+        public class Salt
+        {
+            public static string Create()
+            {
+                byte[] randomBytes = new byte[128 / 8];
+                using (var generator = RandomNumberGenerator.Create())
+                {
+                    generator.GetBytes(randomBytes);
+                }
+
+                return Convert.ToBase64String(randomBytes);
+            }
+        }
+
+
+        private bool ByteArraysEqual(byte[] a, byte[] b)
+        {
+            if (a == b)
+                return true;
+            if (a == null || b == null || a.Length != b.Length)
+                return false;
+            bool flag = true;
+            for (int index = 0; index < a.Length; ++index)
+                flag &= a[index] == b[index];
+            return flag;
+        }
+
+
+        public string HashPassword(string password)
+        {
+            if (password == null)
+                throw new ArgumentNullException(nameof(password));
+            byte[] salt;
+            byte[] bytes;
+            using (Rfc2898DeriveBytes rfc2898DeriveBytes = new Rfc2898DeriveBytes(password, 16, 1000))
+            {
+                salt = rfc2898DeriveBytes.Salt;
+                bytes = rfc2898DeriveBytes.GetBytes(32);
+            }
+            byte[] inArray = new byte[49];
+            Buffer.BlockCopy(salt, 0, inArray, 1, 16);
+            Buffer.BlockCopy(bytes, 0, inArray, 17, 32);
+            return Convert.ToBase64String(inArray);
+        }
+
+        public bool VerifyHashedPassword(string hashedPassword, string password)
+        {
+            if (hashedPassword == null)
+                return false;
+            if (password == null)
+                throw new ArgumentNullException(nameof(password));
+            byte[] numArray = Convert.FromBase64String(hashedPassword);
+            if (numArray.Length != 49 || numArray[0] != 0)
+                return false;
+            byte[] salt = new byte[16];
+            Buffer.BlockCopy(numArray, 1, salt, 0, 16);
+            byte[] a = new byte[32];
+            Buffer.BlockCopy(numArray, 17, a, 0, 32);
+            byte[] bytes;
+            using (Rfc2898DeriveBytes rfc2898DeriveBytes = new Rfc2898DeriveBytes(password, salt, 1000))
+                bytes = rfc2898DeriveBytes.GetBytes(32);
+
+
+            return ByteArraysEqual(a, bytes);
+        }
+
+
+        public string Encrypt(string key, string data)
+        {
+            string encData = null;
+            byte[][] keys = GetHashKeys(key);
+
+            try
+            {
+                encData = EncryptStringToBytes_Aes(data, keys[0], keys[1]);
+            }
+            catch (CryptographicException) { }
+            catch (ArgumentNullException) { }
+
+            return encData;
+        }
+
+        public string Decrypt(string key, string data)
+        {
+            string decData = null;
+            byte[][] keys = GetHashKeys(key);
+
+            try
+            {
+                decData = DecryptStringFromBytes_Aes(data, keys[0], keys[1]);
+            }
+            catch (CryptographicException) { }
+            catch (ArgumentNullException) { }
+
+            return decData;
+        }
+
+        private byte[][] GetHashKeys(string key)
+        {
+            byte[][] result = new byte[2][];
+            Encoding enc = Encoding.UTF8;
+
+            SHA256 sha2 = new SHA256CryptoServiceProvider();
+
+            byte[] rawKey = enc.GetBytes(key);
+            byte[] rawIV = enc.GetBytes(key);
+
+            byte[] hashKey = sha2.ComputeHash(rawKey);
+            byte[] hashIV = sha2.ComputeHash(rawIV);
+
+            Array.Resize(ref hashIV, 16);
+
+            result[0] = hashKey;
+            result[1] = hashIV;
+
+            return result;
+        }
+
+        //source: https://msdn.microsoft.com/de-de/library/system.security.cryptography.aes(v=vs.110).aspx
+        private static string EncryptStringToBytes_Aes(string plainText, byte[] Key, byte[] IV)
+        {
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            byte[] encrypted;
+
+            using (AesManaged aesAlg = new AesManaged())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt =
+                            new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+            return Convert.ToBase64String(encrypted);
+        }
+
+        //source: https://msdn.microsoft.com/de-de/library/system.security.cryptography.aes(v=vs.110).aspx
+        private static string DecryptStringFromBytes_Aes(string cipherTextString, byte[] Key, byte[] IV)
+        {
+            byte[] cipherText = Convert.FromBase64String(cipherTextString);
+
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            string plaintext = null;
+
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt =
+                            new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return plaintext;
+        }
+
+
+
+    }
+
+}
